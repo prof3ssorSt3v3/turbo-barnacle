ab055494a9f6bdfe796baf693c76e95f813e8f04
Prof3ssorSt3v3 <griffis@algonquincollege.com>
2023-11-25 23:14:48 -0500

    add 4 base endpoints

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,54 @@
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
 const router = express.Router();
 
+function createcode() {
+  //create the 4 digit code
+  return []
+    .fill(0, 0, 3)
+    .map((v) => Math.floor(Math.random() * 10).toString())
+    .join('');
+}
+
+function createSession() {
+  return uuidv4();
+}
+
 router.get('/', (request, response) => {
-  response.send('Hello from the Movie Night API!');
+  response.status(200).json({
+    data: {
+      message: 'Hello from the Movie Night API!',
+      '/start-session': ['requires {String device_id}', 'returns {data: {String message, String session_id }}'],
+      '/join-session': ['requires {String device_id, int code}', 'returns {data: {String message, String session_id }}'],
+      '/vote-movie': ['requires {String session_id, int movie_id, Boolean vote}', 'returns {data: {String message, int movie_id, Boolean match}}'],
+    },
+  });
+  //TODO: add the list of API endpoints
 });
 
 // Create a new session
-router.post('/session', async (req, res) => {
-  res.status(201).json({ data: 'new session created.' });
+router.post('/start-session', async (req, res) => {
+  //requires {String device_id}
+  //returns {data: {String message, String session_id }}
+
+  res.status(201).json({ data: { message: 'new session created.', code: createcode(), session_id: createSession() } });
+});
+
+// Join a session
+router.post('/join-session', async (req, res) => {
+  //requires {String device_id, int code}
+  //returns {data: {String message, String session_id }}
+
+  res.status(201).json({ data: { message: 'new session created.', session_id: createSession() } });
+});
+
+// Vote for movie
+router.post('/vote-movie', async (req, res) => {
+  //requires {String session_id, int movie_id, Boolean vote}
+  //returns {data: {String message, int movie_id, Boolean match}}
+  // if the vote is true then ~25% of the time return true
+  let match = Math.random() * 4 < 1.0 ? true : false;
+  res.status(201).json({ data: { message: 'thanks for voting.', movie_id: req.movie_id, match: match } });
 });
 
 export default router;
