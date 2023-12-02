076c8279b0dcc96d82324d55563492debb2d3f44
Prof3ssorSt3v3 <griffis@algonquincollege.com>
2023-12-01 21:09:31 -0500

    update tests

--- routes/index.js
+++ routes/index.js
@@ -214,7 +214,7 @@ router.get('/vote-movie', (req, res) => {
             if (match == false) {
               //check for other possible winners in the movie_ids array
               console.log(copysessions[index]);
-              let movieVotes = copysessions[index].movie_ids.entries();
+              let movieVotes = Object.entries(copysessions[index]['movie_ids']);
               for (const [m, v] of movieVotes) {
                 //this loop will be in the order that movie ids were added to the array
                 console.log(m, v);
